22e2769b346bde623130619ab32b74860c538f5b
BryanLS736 <bryanlserna07@gmail.com>
2026-06-11 10:33:13 -0500

    Agregación de las primeras funciones

--- database/consultas/2_consultas-funciones/4_funciones-cadena.sql
+++ database/consultas/2_consultas-funciones/4_funciones-cadena.sql
@@ -1,37 +1,37 @@
 USE clinica_db;
 
--- CONCAT(): Une nombres y apellidos para mostrar
+-- CONCAT(): Une nombres y apellidos para formar el nombre completo en una sola columna
 SELECT CONCAT(nombres, ' ', apellidos) AS nombre_completo
 FROM paciente;
 
--- LENGTH(): 
+-- LENGTH(): Muestra la cantidad de caracteres del nombre del médico
 SELECT nombres, LENGTH(nombres) AS longitud_nombre
 FROM medico;
 
--- SUBSTRING(): 
+-- SUBSTRING(): Muestra los primeros 4 dígitos del DNI del paciente
 SELECT dni, SUBSTRING(dni, 1, 4) AS primeros_digitos
 FROM paciente;
 
--- LOWER():
+-- LOWER(): Convierte el correo a minúsculas
 SELECT LOWER(correo) AS correo_minusculas
 FROM medico;
 
--- UPPER(): 
+-- UPPER(): Convierte el nombre de la especialidad a mayúsculas
 SELECT UPPER(nombre_especialidad) AS especialidad_mayusculas
 FROM especialidad;
 
--- LTRIM():
+-- LTRIM(): Elimina los espacios en blanco al inicio de la dirección
 SELECT LTRIM(direccion_cliente) AS direccion_sin_espacios_izq
 FROM paciente;
 
--- RTRIM()
+-- RTRIM(): Elimina los espacios en blanco al final del teléfono
 SELECT RTRIM(telefono) AS telefono_sin_espacios_der
 FROM recepcionista;
 
--- CAST()
+-- CAST(): Convierte la fecha a tipo CHAR
 SELECT id_cita, CAST(fecha AS CHAR) AS fecha_texto
 FROM cita;
 
--- CONVERT()
+-- CONVERT(): Convierte la hora a tipo CHAR
 SELECT id_cita, CONVERT(hora, CHAR) AS hora_texto
 FROM cita;
